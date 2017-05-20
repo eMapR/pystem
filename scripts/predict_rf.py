1e971812eed12e019e37b76d52f78de95363fabc
smHooper <hoopersa@oregonstate.edu>
2017-05-20 13:47:23 -0700

    optionally aggregate by mode for regressor

--- scripts/predict_rf.py
+++ scripts/predict_rf.py
@@ -9,9 +9,12 @@ import cPickle as pickle
 import matplotlib.pyplot as plt
 import matplotlib
 from osgeo import gdal
+from multiprocessing import Pool
 
 import randomforest as forest
 from stem import evaluate_ebird, evaluate_by_lc
+import stem
+import mosaic_by_tsa as mosaic
 
 matplotlib.style.use('ggplot')
 
@@ -25,7 +28,7 @@ def parse_constant_vars(constant_vars):
     return var_dict
 
 
-def main(params, n_pieces=False, ydims=None, constant_vars=None, year=''):
+def main(params, n_pieces=False, ydims=None, constant_vars=None, year='', agg_method=None):
 
     t0 = time.time()
     print 'Predicting Random Forest... %s\n' % time.ctime(t0)
@@ -61,13 +64,13 @@ def main(params, n_pieces=False, ydims=None, constant_vars=None, year=''):
     # Make sure vars are sorted alphabetically since they were for training
     pred_vars  = sorted(df_var.index)
     df_var = df_var.reindex(pred_vars)
-    if 'constant_vars' in inputs:
+    '''if 'constant_vars' in inputs:
         constant_vars = parse_constant_vars(constant_vars)
         #year = constant_vars['YEAR']
         year = 2012
         pred_constants = sorted(constant_vars.keys())
     else:
-        df_var.search_str = [s.format(2007) for s in df_var.search_str]
+        df_var.search_str = [s.format(2007) for s in df_var.search_str]'''
 
     #out_dir = os.path.dirname(out_raster)
     if not os.path.exists(out_dir): os.mkdir(out_dir)
@@ -97,23 +100,22 @@ def main(params, n_pieces=False, ydims=None, constant_vars=None, year=''):
     # Get mask and raster info
     ds = gdal.Open(mask_path)
     ar = ds.ReadAsArray()
-    mask = ar != 0
-    ar = None
+    nodata_mask = ar != 0
     xsize = ds.RasterXSize
     ysize = ds.RasterYSize
     tx = ds.GetGeoTransform()
     prj = ds.GetProjection()
     driver = gdal.GetDriverByName('gtiff')
-    ul_x, x_res, _, ul_y, _, y_res = tx
-    ds = None
+    ul_x, x_res, x_rot, ul_y, y_rot, y_res = tx
+    
 
     # Predict
-    'Predicting with %s processors... %s' % (rf_model.n_jobs, time.ctime(time.time()))
+    #print 'Predicting with %s processors... %s' % (rf_model.n_jobs, time.ctime(time.time()))
     t1 = time.time()
     predict_pieces = []
     
-    if n_pieces:
-        # assumes predictors all line up and have same dimensions'''
+    '''if n_pieces:
+        # assumes predictors all line up and have same dimensions
         if 'mask_path' not in inputs: 
             raise NameError('mask_path not specified')
         # Figure out the y dimension of each piece
@@ -123,6 +125,7 @@ def main(params, n_pieces=False, ydims=None, constant_vars=None, year=''):
         lower_ydim = range(piece_ysize, ysize, piece_ysize)
         lower_ydim[-1] = ysize
         ydims = zip(upper_ydim, lower_ydim)
+        
         for i, yd in enumerate(ydims):
             print 'Predicting for piece %s of %s...' % (i + 1, n_pieces)
             t1 = time.time()
@@ -136,8 +139,87 @@ def main(params, n_pieces=False, ydims=None, constant_vars=None, year=''):
             print 'Total time for this piece: %.1f minutes\n' % ((time.time() - t1)/60)
             del ar_predictors, nodata_mask, ar_prediction
         ar_prediction = np.concatenate(predict_pieces)
-        del predict_pieces
+        del predict_pieces'''
     
+    if 'n_tiles' not in inputs:
+        print 'n_tiles not specified. Using default: 25 x 15 ...\n'
+        n_tiles = 25, 15
+    else:
+        n_tiles = [int(i) for i in n_tiles.split(',')]
+        
+    if 'n_tiles' in inputs:
+        df_tiles, df_tiles_rc, tile_size = stem.get_tiles(n_tiles, xsize, ysize, tx)
+        stem.coords_to_shp(df_tiles, '/vol/v2/stem/extent_shp/CAORWA.shp', os.path.join(out_dir, 'tile.shp'))
+        empty_tiles = []
+        ar_out = np.full((ysize, xsize), nodata, dtype=np.uint8)
+        tile_dir = os.path.join(out_dir, 'predict_tiles')
+        if not os.path.isdir(tile_dir):
+            os.mkdir(tile_dir)
+        for i, (ind, tile_coords) in enumerate(df_tiles.iterrows()):
+            print 'Predicting for tile %s of %s...' % (i + 1, len(df_tiles))
+            t1 = time.time()
+            coords = tile_coords[['ul_x', 'ul_y', 'lr_x', 'lr_y']].tolist()
+            tsa_ar, tsa_off = mosaic.extract_kernel(ds, 1, coords, tx, xsize, ysize, nodata=nodata)
+            tsa_mask = tsa_ar == 0
+            if tsa_mask.all():
+                print 'Tile %s empty. Skipping...' % ind
+                continue
+            tsa_ar[tsa_mask] = nodata
+            # Get the ids of TSAs this kernel covers
+            tsa_ids = np.unique(tsa_ar)
+            tsa_strs = ['0' + str(tsa) for tsa in tsa_ids if tsa!=nodata]
+            array_shape = tsa_ar.shape
+        
+            # Get an array of predictors where each column is a flattened 2D array of a
+            #   single predictor variable
+            temp_nodata = -9999
+            ar_predictors = stem.get_predictors(df_var, tx, tsa_strs, tsa_ar, coords, tsa_mask, temp_nodata, 1)
+            nodata_mask = ~ np.any(ar_predictors==temp_nodata, axis=1)
+            predictors = ar_predictors[nodata_mask]
+            t2 = time.time()
+            if agg_method == 'mode':
+                args = []
+                for dt in rf_model.estimators_:
+                    args.append([dt, predictors])
+                pool = Pool(rf_model.n_jobs)
+                t3 = time.time()
+                dt_predictions = np.vstack(pool.map(forest.par_predict_from_dt, args, 1))
+                print 'Prediction time: %.1f minutes' % ((time.time() - t3)/60)
+                t3 = time.time()
+                predictions = stem.mode(dt_predictions, axis=0)
+                print 'Aggregation time:  %.1f minutes' % ((time.time() - t3)/60)
+                del dt_predictions
+                t3 = time.time()
+                pool.close()
+                pool.join()
+                print 'Closing time:  %.1f minutes' % ((time.time() - t3)/60)
+            else:
+                predictions = rf_model.predict(ar_predictors[nodata_mask])
+            print 'Prediction time: %.1f minutes' % ((time.time() - t2)/60)
+            
+            ar_tile = np.full(ar_predictors.shape[0], nodata, dtype=np.uint8)
+            ar_tile[nodata_mask] = predictions.astype(np.uint8)
+            ul_r, lr_r, ul_c, lr_c = df_tiles_rc.ix[ind]
+            ar_out[ul_r : lr_r, ul_c : lr_c] = ar_tile.reshape(array_shape)
+            tx_tile = tile_coords.ul_x, x_res, x_rot, tile_coords.ul_y, y_rot, y_res
+            mosaic.array_to_raster(ar_tile.reshape(array_shape), tx_tile, prj, driver, os.path.join(tile_dir, 'tile_%s.tif' % ind), dtype=gdal.GDT_Byte, nodata=nodata)
+            print 'Total time for this piece: %.1f minutes\n' % ((time.time() - t1)/60)
+            #del ar_predictors, nodata_mask, ar_prediction'''
+        #ar_prediction = np.concatenate(predict_pieces)
+        #del predict_pieces
+        '''ar_out = np.full((ysize, xsize), nodata, dtype=np.uint8)
+        for ind, tile_coords in df_tiles_rc.iterrows():
+            if ind in empty_tiles:
+                continue
+            ul_r, lr_r, ul_c, lr_c = tile_coords
+            tile_file = os.path.join(tile_dir, 'tile_%s.tif' % ind)
+            if not os.path.exists(tile_file):
+                continue
+            ds_t = gdal.Open(tile_file)
+            ar_tile = ds_t.ReadAsArray()
+            t_ulx = df_tiles.ix[ind, ['ul_x', 'ul_y']]
+            ar_out[ul_r : lr_r, ul_c : lr_c] = ar_tile'''
+        
     else:
         ar_predictors, nodata_mask = forest.get_predictors(df_var, nodata)
         # If the predictions are too large (i.e. cause memory errors), split the predictor array into pieces and predict
@@ -161,12 +243,14 @@ def main(params, n_pieces=False, ydims=None, constant_vars=None, year=''):
 
     # Save the prediction array to disk
     stamp = os.path.basename(out_dir)
-    out_path = os.path.join(out_dir, 'final_%s.tif' % stamp)
-    ar_prediction = ar_prediction.reshape(ysize, xsize)
+    out_path = os.path.join(out_dir, '%s_rf_mean.tif' % stamp)
+    #ar_prediction = ar_prediction.reshape(ysize, xsize)
     if constant_vars: 
         out_path = out_path.replace('.tif', '_yr%s.tif' % year )
-    forest.array_to_raster(ar_prediction, tx, prj, driver, out_path, gdal.GDT_Byte, nodata)#"""
-    
+    forest.array_to_raster(ar_out, tx, prj, driver, out_path, gdal.GDT_Byte, nodata)#"""
+    # Delete the tiles
+    shutil.rmtree(tile_dir)
+    ds = None
     '''stamp = os.path.basename(out_dir)
     path = os.path.join(out_dir, 'final_%s_yr2011.tif' % stamp) 
     stamp = os.path.basename(os.path.dirname(path))
@@ -203,7 +287,21 @@ def main(params, n_pieces=False, ydims=None, constant_vars=None, year=''):
         df, samples, roc_curves = evaluate_ebird(sample_txt, ar_prediction, tx,
                                                  cell_size, target_col, n_per_cell,
                                                  n_trials, year)
-                                                 
+        if len(roc_curves) > 0:
+            for fpr, tpr, thresholds in roc_curves:
+                plt.plot(fpr, tpr, 'k', alpha=.1)
+            out_png = os.path.join(out_dir, '{0}_roc_curve_{1}.png'.format(stamp, year))
+            plt.savefig(out_png)
+            
+        if 'lc_path' in test_dict:
+            '''df_lc = evaluate_by_lc(samples, ar_prediction, lc_path, target_col)
+            out_txt = os.path.join('/vol/v2/stem/ebird/results/performance_by_lc', '{0}_eval_{1}_land_cover.txt'.format(stamp, year))
+            df_lc.to_csv(out_txt, sep='\t')'''
+        
+        #df_samples = pd.read_csv(sample_txt, sep='\t', index_col='obs_id')
+        df_lc = evaluate_by_lc(samples, ar_prediction, lc_path, target_col)
+        out_txt = os.path.join(out_dir, '{0}_eval_{1}_land_cover_all_samples.txt'.format(stamp, year))
+        df_lc.to_csv(out_txt, sep='\t')
         if 'inventory_txt' in test_dict:
             score_cols = sorted(df.columns)
             df_inv = pd.read_csv(inventory_txt, sep='\t', index_col='stamp') 
@@ -219,22 +317,6 @@ def main(params, n_pieces=False, ydims=None, constant_vars=None, year=''):
     else:
         print '\nEither "test_samples" or "inventory_txt" was not specified.' +\
             ' This model will not be evaluated...'
-    
-    if len(roc_curves) > 0:
-        for fpr, tpr, thresholds in roc_curves:
-            plt.plot(fpr, tpr, 'k', alpha=.1)
-        out_png = os.path.join(out_dir, '{0}_roc_curve_{1}.png'.format(stamp, year))
-        plt.savefig(out_png)
-    
-    if 'lc_path' in test_dict:
-        '''df_lc = evaluate_by_lc(samples, ar_prediction, lc_path, target_col)
-        out_txt = os.path.join('/vol/v2/stem/ebird/results/performance_by_lc', '{0}_eval_{1}_land_cover.txt'.format(stamp, year))
-        df_lc.to_csv(out_txt, sep='\t')'''
-        
-        #df_samples = pd.read_csv(sample_txt, sep='\t', index_col='obs_id')
-        df_lc = evaluate_by_lc(samples, ar_prediction, lc_path, target_col)
-        out_txt = os.path.join(out_dir, '{0}_eval_{1}_land_cover_all_samples.txt'.format(stamp, year))
-        df_lc.to_csv(out_txt, sep='\t')
 
     print '\nTotal runtime: %.1f minutes' % ((time.time() - t0)/60)
 
