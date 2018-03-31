e770cc59490aac57df422fea75f47dab5cb8228d
smHooper <hoopersa@oregonstate.edu>
2018-03-30 18:03:31 -0700

    change rm to mv

--- scripts/utils/loop_confusion_matrix.sh
+++ scripts/utils/loop_confusion_matrix.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
+
+
+# Make new dir to store processed files if it doesn't already exist
+mv_dir=$1"processed"
+if [ ! -d "$mv_dir" ]; then
+	mkdir $mv_dir
+fi
+
+# Loop through and run the script for each param file found
 for file in $( find $1 -type f -name "confusion_params_*.txt" )
 do
-  python /vol/v2/stem/stem-git/scripts/confusion_matrix.py $file
-  rm $file
+  python /vol/v2/stem/stem-git/scripts/evaluation/confusion_matrix.py $file
+  mv "$file" "$mv_dir"
 done
